b6af379cb00ed895e583639f28cfa77d6552b50f
EvaSpessotto <eva@spessotto.com>
2019-03-22 17:49:54 +0100

    Add custom elements

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,6 @@ import "semantic-ui-less/semantic.less";
 const layout = ({ children, isHome, path }) => {
   return (
     <div>
-      <div>I am the layout component</div>
         {children}
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,78 @@
 import React from "react";
-import Layout from '../components/Layout';
 import 'semantic-ui-css/semantic.min.css'
+import Layout from '../components/Layout';
+import { Grid, Container, Button, Image, Card, Icon } from 'semantic-ui-react';
+
 
 
 export default () => (
   <Layout>
-    Hello there !
+    <Grid as={Container} style={{ marginTop: '50px', marginBottom:'50px' }} text>
+      <Grid.Row>
+        <Grid.Column>
+          <Image src="https://images.pexels.com/photos/921294/pexels-photo-921294.png?auto=compress&cs=tinysrgb&h=750&w=1260" />
+          <h1>This is my title</h1>
+          <p>Ex non tempor mollit culpa velit exercitation ea sunt veniam irure sint cupidatat in elit. Irure anim dolore cupidatat consectetur laboris laboris deserunt do elit excepteur voluptate. Est esse magna cupidatat aliqua veniam occaecat ex sit consectetur et quis deserunt ea in. Magna consequat exercitation eu dolor aliqua exercitation nisi ullamco veniam dolore amet.</p>
+          <p>Ex non tempor mollit culpa velit exercitation ea sunt veniam irure sint cupidatat in elit. Irure anim dolore cupidatat consectetur laboris laboris deserunt do elit excepteur voluptate. Est esse magna cupidatat aliqua veniam occaecat ex sit consectetur et quis deserunt ea in. Magna consequat exercitation eu dolor aliqua exercitation nisi ullamco veniam dolore amet.</p>
+          <p>Ex non tempor mollit culpa velit exercitation ea sunt veniam irure sint cupidatat in elit. Irure anim dolore cupidatat consectetur laboris laboris deserunt do elit excepteur voluptate. Est esse magna cupidatat aliqua veniam occaecat ex sit consectetur et quis deserunt ea in. Magna consequat exercitation eu dolor aliqua exercitation nisi ullamco veniam dolore amet.</p>
+          <Button>Click me</Button>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+    <Grid as={Container}>
+      <Grid.Row>
+        <Grid.Column>
+          <Card.Group itemsPerRow={2}>
+          <Card as={Grid}>
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <Image src='https://www.blendswap.com/files/images/2014/02/image72542/medium_ab2c6bd80f9de6167809cd7fd7598a4d.jpg' />
+                </Grid.Column>
+                <Grid.Column>
+                  <Card.Content>
+                    <Card.Header><h2>Super title</h2></Card.Header>
+                    <Card.Meta>
+                      <span className='date'>Eva Spessotto, 22 march 2019</span>
+                    </Card.Meta>
+                    <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='user' />
+                      22 Friends
+                    </a>
+                  </Card.Content>
+                </Grid.Column>
+              </Grid.Row>
+            </Card>
+
+            <Card as={Grid}>
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <Image src='https://www.blendswap.com/files/images/2014/02/image72542/medium_ab2c6bd80f9de6167809cd7fd7598a4d.jpg' />
+                </Grid.Column>
+                <Grid.Column>
+                  <Card.Content>
+                    <Card.Header><h2>Super title</h2></Card.Header>
+                    <Card.Meta>
+                      <span className='date'>Eva Spessotto, 22 march 2019</span>
+                    </Card.Meta>
+                    <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='user' />
+                      22 Friends
+                    </a>
+                  </Card.Content>
+                </Grid.Column>
+              </Grid.Row>
+            </Card>
+          </Card.Group>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+
   </Layout>
 
 )
